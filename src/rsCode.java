153cf372217db2f0f3d8ce28d1c7efeb16f273e1
yeshen4328 <yeshen4328@163.com>
2016-06-20 16:21:20 +0800

    master

--- src/rsCode.java
+++ src/rsCode.java
@@ -8,7 +8,7 @@ public class rsCode
 {  
     private static final int MM = 8;  
     private static final int NN = 255;  
-    private static final int KK = 127;  
+    private static final int KK = 35;  
     private static final int TT = (NN - KK) / 2; 
     
     private int[] pp = {1,0,1,1,1,0,0,0,1}  ;//  N255K127{1,0,1,1,1,0,0,0,1}    N15K11M4{1,1,0,0,1}
@@ -457,7 +457,7 @@ public class rsCode
 			byte[] data = null;
 			int[] dataNew = null;
 			try {
-					fis = new FileInputStream("./introduction.txt");
+					fis = new FileInputStream("./surface.txt");
 					int size = fis.available();
 					//size = 6400/MM/NN*KK*MM;
 					data = new byte[size];
@@ -492,7 +492,7 @@ public class rsCode
 				else
 					writeToFile[i] = 0;
 			try {
-					FileWriter fw = new FileWriter("./msg_introRs15_11.txt");
+					FileWriter fw = new FileWriter("./msg_surface255_35.txt");
 					for(int i = 0;i < writeToFile.length; i++)
 					{
 						byte[] b = byteArr2DoubleRadix(writeToFile[i]);
